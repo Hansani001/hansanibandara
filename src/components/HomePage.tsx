909cb8dac760687276e6d016f8fa6b3e8a71161c
Hansani001 <115695269+Hansani001@users.noreply.github.com>
2025-06-30 09:05:28 +0530

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Linkedin, Mail, Phone } from 'lucide-react';
-import profilePicture from "./profile-picture.jpg";
+import profilePicture from "./profile-picture.JPG";
 
 const HomePage = () => {
   return (
