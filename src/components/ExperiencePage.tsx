5984ae6c1688d9e8db3f56f6a7a19025a5ef6cdd
Hansani001 <115695269+Hansani001@users.noreply.github.com>
2025-11-20 16:13:38 +0530

    Update ExperiencePage.tsx

--- src/components/ExperiencePage.tsx
+++ src/components/ExperiencePage.tsx
@@ -4,15 +4,27 @@ import React from 'react';
 const ExperiencePage = () => {
   const experiences = [
     {
+
+
       id: 1,
+      company: ' Uga Resorts',
+      position: 'System Engineer - Information Technology',
+      location: 'Sri Lanka',
+      period: 'Nov 2024 - Present ',
+      description: 'Gained hands-on experience in project management by supporting both the Millennium Technology Platform (MTP) and Development Infrastructure teams. Assisted project managers across all SDLC phases, contributing to planning, implementation, and monitoring. Maintained project documentation including burn down charts, Power BI dashboards, and status reports.Coordinated with testers for smooth infrastructure rollouts and used Jira for clarity code and release tracking. Facilitated agile ceremonies—Daily Scrums, Sprint Planning, Reviews, and Retrospectives—and documented meetings using Confluence with clear agendas and action items. Actively contributed to process improvement efforts, enhancing project efficiency and execution.'
+    },
+
+
+     {  
+      id: 2,
       company: 'Spa Ceylon Ayurveda Wellness',
       position: 'Junior Executive,Digital Transformation',
       location: 'Sri Lanka',
-      period: 'Nov 2024 - Present',
+      period: 'Nov 2024 - Nov 2025',
       description: 'Coordinated digital transformation projects by collaborating with cross-functional teams and brand partners. Gathered technical and functional requirements, analyzed data to support decision-making, and identified opportunities for process improvement. Assisted in documenting solutions, training staff, and providing technical support for new digital systems.'
     },
     {
-      id: 2,
+      id:3,
       company: ' London Stock Exchange Group (LSEG)',
       position: 'Intern, Project Management',
       location: 'Sri Lanka',
@@ -20,7 +32,7 @@ const ExperiencePage = () => {
       description: 'Gained hands-on experience in project management by supporting both the Millennium Technology Platform (MTP) and Development Infrastructure teams. Assisted project managers across all SDLC phases, contributing to planning, implementation, and monitoring. Maintained project documentation including burn down charts, Power BI dashboards, and status reports.Coordinated with testers for smooth infrastructure rollouts and used Jira for clarity code and release tracking. Facilitated agile ceremonies—Daily Scrums, Sprint Planning, Reviews, and Retrospectives—and documented meetings using Confluence with clear agendas and action items. Actively contributed to process improvement efforts, enhancing project efficiency and execution.'
     },
     {
-      id: 3,
+      id: 4,
       company: 'Hong Kong and Shanghai Banking Corporation Limited (HSBC)',
       position: 'Customer Solutions Representative',
       location: 'Sri Lanka',
